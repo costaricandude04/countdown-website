9929f5e1b3eec0c4b1306a5dcb6a68883cbbf3df
costaricandude04 <123607755+costaricandude04@users.noreply.github.com>
2025-04-17 20:37:49 +0000

    website update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('01/01/2026').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Awesome Game' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index', 
+    { title: 'My Awesome Game', countDown: numberOfMilliseconds }
+  );
 });
 
 module.exports = router;
